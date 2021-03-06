978c31911bf260843bb58a64877ebd6c55b8e139
Alireza Sadeghi <aliraza7926@gmail.com>
2021-03-06 20:49:24 +0330

    add some more test to pages/test.py

--- pages/tests.py
+++ pages/tests.py
@@ -10,3 +10,16 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_url_name(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code,200)
+
+    def test_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
+    
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response,'Homepage')
+    
+    def test_homepage_does_not_contain_incorrect_html(self):
+        response = self.client.get('/')
+        self.assertNotContains(response,'I should not be in this page.')
+        
