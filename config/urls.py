4665040b6086c76f00e56b676feacf024de42c2e
Alireza Sadeghi <aliraza7926@gmail.com>
2021-03-07 21:15:42 +0330

    add signup url to config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
 
     # Local apps
+    path('accounts/',include('accounts.urls')),
     path('',include('pages.urls')),
+
 
 ]
