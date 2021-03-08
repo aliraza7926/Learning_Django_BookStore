5170b36df518af3e381553862df0106746a1a861
Alireza Sadeghi <aliraza7926@gmail.com>
2021-03-08 20:22:02 +0330

    set static config

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILESS_DIR = (str(BASE_DIR.joinpath('static')),)
+STATIC_ROOT = (str(BASE_DIR.joinpath('staticfiles')))
+STATICFILES_FINDERS=[
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 AUTH_USER_MODEL ='accounts.CustomUser'
 
 LOGIN_REDIRECT_URL='home'
