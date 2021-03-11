f7036fc936d99fcff1c4cc606a763482b3481d0f
Alireza Sadeghi <aliraza7926@gmail.com>
2021-03-11 17:33:22 +0330

    add crispy_froms to installed app

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd Party
+    'crispy_forms',
+
     # Local
     'accounts',
     'pages',
@@ -138,3 +141,5 @@ AUTH_USER_MODEL ='accounts.CustomUser'
 
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
