f35fc2a531bb8ce26c595eea7ac160b305ff2b14
Alireza Sadeghi <aliraza7926@gmail.com>
2021-03-11 22:21:15 +0330

    add new test for signup

--- accounts/tests.py
+++ accounts/tests.py
@@ -9,12 +9,12 @@ class CustomUserTests(TestCase):
         User=get_user_model()
         user=User.objects.create_user(
             username="ali",
-            email="ali@mail.com",
+            email="ali@email.com",
             password="a"
         )
 
         self.assertEqual(user.username, "ali")
-        self.assertEqual(user.email, "ali@mail.com")
+        self.assertEqual(user.email, "ali@email.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
@@ -24,15 +24,35 @@ class CustomUserTests(TestCase):
         User=get_user_model()
         admin_user=User.objects.create_superuser(
             username="admin",
-            email="admin@mail.com",
+            email="admin@email.com",
             password="password"
         )
 
         self.assertEqual(admin_user.username, "admin")
-        self.assertEqual(admin_user.email, "admin@mail.com")
+        self.assertEqual(admin_user.email, "admin@email.com")
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
 
 
 
+class SignupTests(TestCase):
+
+    username='newuser'
+    email='newuser@email.com'
+
+    def setUp(self):
+        url = reverse('account_signup')
+        self.response=self.client.get(url)
+    
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code,200)
+        self.assertTemplateUsed(self.response,'account/signup.html')
+        self.assertContains(self.response,'Sign Up')
+        self.assertNotContains(self.response,'I am not be here help me!!! ')
+
+    def test_signup_form(self):
+        new_user=get_user_model().objects.create_user(self.username,self.email)
+        self.assertEqual(get_user_model().objects.all().count(),1)
+        self.assertEqual(get_user_model().objects.all()[0].username,self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email,self.email)
