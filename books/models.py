dde71fad8f6028e2654957c9d98756a95b1e9282
Alireza Sadeghi <aliraza7926@gmail.com>
2021-03-17 17:27:53 +0330

    create review model

--- books/models.py
+++ books/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 import uuid
+
 
 class Book(models.Model):
     id = models.UUIDField(
@@ -10,12 +12,27 @@ class Book(models.Model):
 
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def get_absolute_url(self):
-        return reverse('book_detail',args=[str(self.id)])
-
+        return reverse('book_detail', args=[str(self.id)])
 
     def __str__(self):
         return self.title
-    
+
+
+class Review(models.Model):
+
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review
