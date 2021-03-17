8f718a9b0774e5ae45954b75c656b9ee9f34b4ef
Alireza Sadeghi <aliraza7926@gmail.com>
2021-03-17 17:10:38 +0330

    add some test for books app

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Book
 
-# Create your tests here.
+class BookTests(TestCase):
+    
+    def setUp(self):
+        self.book = Book.objects.create(
+            title='alex rider',
+            author='horis',
+            price='17.98',
+        )
+
+    def test_book_listing(self):
+        self.assertEqual(f'{self.book.title}', 'alex rider')
+        self.assertEqual(f'{self.book.author}', 'horis')
+        self.assertEqual(f'{self.book.price}','17.98')
+    
+    def test_book_list_view(self):
+        response=self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,'alex rider')
+        self.assertTemplateUsed(response,'books/book_list.html')
+    
+    def test_book_detail_view(self):
+        response=self.client.get(self.book.get_absolute_url())
+        no_response=self.client.get('books/12345')
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(no_response.status_code,404)
+        self.assertContains(response,'alex rider')
+        self.assertTemplateUsed(response,'books/book_detail.html')
